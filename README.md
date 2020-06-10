3cd6ec14a8546b9ce0eff452b1b68a99bb474cac
Renat Suleymanov <mail@renat.su>
2020-06-10 11:59:25 +0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # fp-youtube-dl
 FreePascal youtube-dl wrapper
+
+It is a simple freepascal wrapper for awesome youtube-dl [lib](https://github.com/ytdl-org/youtube-dl/). The wrapper is based on CLI (command line interface).
+
+There are crossplatform. You should install the youtube-dl according to [this instruction](https://github.com/ytdl-org/youtube-dl/#installation).
---
+++ youtubedl.pas
@@ -0,0 +1,141 @@
+unit youtubedl;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, process, eventlog
+  ;
+
+type
+
+  { TYoutubeDL }
+
+  TYoutubeDL = class
+  private
+    FHTTPProxyHost: String;
+    FHTTPProxyPassword: String;
+    FHTTPProxyPort: Word;
+    FHTTPProxyUsername: String;
+    FLogger: TEventLog;
+    FOptions: TStrings;
+    FUrl: String;
+    procedure DoLog(aEventType: TEventType; const aMessage: String);
+    procedure SetLogger(AValue: TEventLog);
+    procedure SetOptions(AValue: TStrings);
+    procedure SetUrl(AValue: String);
+    function InternalDownload: Boolean;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    function Download(const aUrl: String = ''): Boolean;
+    property Logger: TEventLog read FLogger write SetLogger;
+    property Url: String read FUrl write SetUrl;   
+    property Options: TStrings read FOptions write SetOptions;
+    property HTTPProxyHost: String read FHTTPProxyHost write FHTTPProxyHost;
+    property HTTPProxyPort: Word read FHTTPProxyPort write FHTTPProxyPort; 
+    property HTTPProxyUsername: String read FHTTPProxyUsername write FHTTPProxyUsername;
+    property HTTPProxyPassword: String read FHTTPProxyPassword write FHTTPProxyPassword;
+
+  end;
+
+implementation
+
+{ TYoutubeDL }
+
+procedure TYoutubeDL.SetUrl(AValue: String);
+begin
+  if FUrl=AValue then Exit;
+  FUrl:=AValue;
+end;
+
+procedure TYoutubeDL.DoLog(aEventType: TEventType; const aMessage: String);
+begin
+  if Assigned(FLogger) then
+    FLogger.Log(aEventType, aMessage);
+end;
+
+procedure TYoutubeDL.SetLogger(AValue: TEventLog);
+begin
+  if FLogger=AValue then Exit;
+  FLogger:=AValue;
+end;
+
+procedure TYoutubeDL.SetOptions(AValue: TStrings);
+begin
+  FOptions.Assign(AValue);
+end;
+
+function TYoutubeDL.Download(const aUrl: String): Boolean;
+begin
+  if aUrl<> EmptyStr then
+    FUrl:=aUrl;
+  Result:=InternalDownload;
+end;
+
+function TYoutubeDL.InternalDownload: Boolean;
+const
+  BUF_SIZE = 2048; // Buffer size for reading the output in chunks
+var
+  aProcess: TProcess;
+  aOutputStream: TMemoryStream;
+  aBuffer       : array[1..BUF_SIZE] of byte;
+  aBytesRead: LongInt;
+  aProxyString: String;
+begin 
+  Result:=False;
+  try
+    aProcess:=TProcess.Create(nil);
+    try
+      aProcess.Executable:={$IFDEF MSWINDOWS}'youtube-dl.exe'{$ENDIF}{$IFDEF UNIX}'youtube-dl'{$ENDIF};
+      aProcess.Parameters.AddStrings(FOptions);
+      aProcess.Parameters.Add(FUrl);
+      if FHTTPProxyHost<>EmptyStr then
+      begin
+        aProxyString:=FHTTPProxyHost+':'+IntToStr(FHTTPProxyPort);
+        if FHTTPProxyUsername<>EmptyStr then
+          aProxyString:=FHTTPProxyUsername+':'+FHTTPProxyPassword+'@'+aProxyString;
+        aProxyString:='https://'+aProxyString;
+        aProcess.Parameters.Add('--proxy');
+        aProcess.Parameters.Add(aProxyString);
+      end;
+      aProcess.Options:=[poUsePipes, poStderrToOutPut, poNoConsole];
+      aProcess.Execute;
+      aOutputStream := TMemoryStream.Create;
+
+      repeat
+        aBytesRead := aProcess.Output.Read(aBuffer{%H-}, BUF_SIZE);
+        aOutputStream.Write(aBuffer, aBytesRead)
+      until aBytesRead = 0;
+
+      with TFileStream.Create('~youtube-output.txt', fmCreate) do
+      begin
+        aOutputStream.Position := 0; // Required to make sure all data is copied from the start
+        CopyFrom(aOutputStream, aOutputStream.Size);
+        Free
+      end;
+      aOutputStream.Free;
+    finally
+      aProcess.Free;
+    end;
+    Result:=True;
+  except
+    on E: Exception do
+      DoLog(etError, E.ClassName+': '+E.Message);
+  end;
+end;
+
+constructor TYoutubeDL.Create;
+begin
+  FOptions:=TStringList.Create;
+end;
+
+destructor TYoutubeDL.Destroy;
+begin
+  FOptions.Free;
+  inherited Destroy;
+end;
+
+end.
+
